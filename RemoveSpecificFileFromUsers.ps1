c3c581579d2da5fdc8fb10c19d4a90249ee7c1a0
Vik Winchester <116097638+vikwinchester@users.noreply.github.com>
2024-07-11 08:52:58 +0300

    Add files via upload

---
+++ RemoveSpecificFileFromUsers.ps1
@@ -0,0 +1,10 @@
+﻿$users = Get-ChildItem -Path "C:\Users"
+
+foreach($user in $users){
+  $path = "C:\Users\$($user.name)\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+    if(test-path $path){
+        Remove-Item -Path "C:\Users\$($user.name)\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start.bat" -Force
+        Remove-Item -Path  C:\Users\$($user.name)\AppData\Local\remove_appx.ps1 -Force
+
+        }
+}
